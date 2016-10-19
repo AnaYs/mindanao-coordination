c5554edcb2d20eda83015b60c6dd7f2604db240a
Anna Isabel Lamers <isabel.lamers@gmail.com>
2016-10-19 19:14:28 +0800

    basic crud for adding, editing and diplaying programs

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,19 +1,39 @@
 class ProgramsController < ApplicationController
+  before_action :set_program, only: [:show, :edit, :update]
+
   def index
+    @programs = Program.all
   end
 
   def show
   end
 
   def new
+    @program = Program.new
   end
 
   def create
+    @program = Program.new(program_params)
+    @program.save
+
+    redirect_to program_path(@program)
   end
 
   def edit
   end
 
   def update
+    @program.update(program_params)
+    redirect_to program_path(@program)
+  end
+
+private
+
+  def set_program
+    @program = Program.find(params[:id])
+  end
+
+  def program_params
+    params.require(:program).permit(:name, :main_office, :area_of_work, :organisation, :latitude, :longitude)
   end
 end
