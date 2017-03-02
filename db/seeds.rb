a0da912d759bba81bd5014bc3594acd406798c8d
Anna Isabel Lamers <isabel.lamers@gmail.com>
2017-03-02 12:04:37 +0100

    seeds file and users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+coseram = Program.create({
+  name: "COSERAM",
+  main_office: "Butuan",
+  area_of_work: "Resource-based conflicts, overlapping planning and titling regimes, governance of natural resources, IP, CADT ADSDPP",
+  organisation: "GIZ",
+})
+
+Project.create({
+  name: "Peace building and development needs (PBDN)",
+  main_office: "Butuan",
+  area_of_work: "resource-based conflicts",
+  organisation: "GIZ",
+  program: coseram
+})
+
+User.create({email: "test@test.com", password: 12345678})
+# forum = Program.create({
+#   name: "Dialogue for Peace",
+#   main_office: ,
+#   area_of_work: ,
+#   organisation: "forumZFD"
+# })
+
+# cps = Program.create({
+#   name: ,
+#   main_office: ,
+#   area_of_work: ,
+#   organisation: "Civil Peace Service"
+# })
+
+# kas = Program.create({
+#   name: ,
+#   main_office: ,
+#   area_of_work: ,
+#   organisation: "Konrad-Adenauer-Stiftung"
+# })
